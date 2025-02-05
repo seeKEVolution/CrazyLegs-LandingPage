6f220f82b1462217f85d75e767101d54656228c2
Kev <kevin@seekevolution.co>
2025-02-05 12:36:48 -0500

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,43 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: 'Crazy Legs',
   description: 'Coming Soon to Charlotte',
+  icons: {
+    icon: [
+      {
+        url: "/images/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/images/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/images/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      }
+    ],
+    apple: {
+      url: "/images/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/images/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome",
+        url: "/images/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
